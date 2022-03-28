0ad6bd749489cc346293675b93195e70860a9282
Prakat Sherchan <sherchan001@gannon.edu>
2022-03-28 06:49:06 -0400

    bluetooth scan, paired device, connect added

--- app/src/main/java/com/sherchan/pbe2/MainActivity.java
+++ app/src/main/java/com/sherchan/pbe2/MainActivity.java
@@ -1,27 +1,171 @@
 package com.sherchan.pbe2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+//import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.Fragment;
+import androidx.activity.ComponentActivity;
+
+//import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+
 import androidx.viewpager.widget.ViewPager;
+
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
+import android.bluetooth.BluetoothDevice;
+
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
 
 public class MainActivity extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+    private static final int REQUEST_ENABLE_BT = 0;
+    private static final int REQUEST_DISCOVER_BT = 1;
+    static final UUID mUUID = UUID.fromString("0000101-0000-1000-8000-00805F9B34FB");
 
-            ViewPager viewPager = findViewById(R.id.viewPager);
+    static final int STATE_LISTENING = 1;
+    static final int STATE_CONNECTING=2;
+    static final int STATE_CONNECTED=3;
+    static final int STATE_CONNECTION_FAILED=4;
 
-            AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager());
-            pagerAdapter.addFragmet(new LoginFragment());
-            pagerAdapter.addFragmet(new RegisterFragment());
-            viewPager.setAdapter(pagerAdapter);
+    int REQUEST_ENABLE_BLUETOOTH=1;
+
+    Button search, connect, paired;
+    ListView list;
+    TextView mStatusBlueTv;
+    ArrayList<String> bList = new ArrayList<String>();
+    ArrayAdapter<String> aAdapter;
+    TextView textView;
+
+    BluetoothDevice[] btArray;
+
+    BluetoothAdapter mBadapter = BluetoothAdapter.getDefaultAdapter();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        search = findViewById(R.id.search);
+        connect = findViewById(R.id.connect);
+        paired = findViewById(R.id.pairedBtn);
+        mStatusBlueTv = findViewById(R.id.statusBluetoothTv);
+
+        list = findViewById(R.id.list);
+
+        Thread2 t = new Thread2();
+        t.start();
+
+        if (mBadapter == null) {
+            mStatusBlueTv.setText("Bluetooth is not available");
+        } else {
+            mStatusBlueTv.setText("Bluetooth is available");
         }
+
+        if(!mBadapter.isEnabled()){
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BLUETOOTH);
+        }
+
+        exeButton();
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBadapter.startDiscovery();
+            }
+        });
+
+        IntentFilter intentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(myReceiver, intentFilter);
+
+        aAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, bList);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragmet(new LoginFragment());
+        pagerAdapter.addFragmet(new RegisterFragment());
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    BroadcastReceiver myReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                bList.add(device.getName());
+                    aAdapter.notifyDataSetChanged();
+                }
+            }
+        };
+
+    Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message message) {
+            textView.setText(String.valueOf(message.arg1));
+            return false;
+        }
+    });
+
+    private void exeButton() {
+        paired.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Set<BluetoothDevice> bt = mBadapter.getBondedDevices();
+                String[] strings = new String[bt.size()];
+                int index= 0;
+
+                if(bt.size() > 0) {
+                    for(BluetoothDevice device:bt){
+                        btArray[index] = device;
+                        strings[index] = device.getName();
+                        index++;
+                    }
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, strings);
+                    list.setAdapter(arrayAdapter);
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+            switch(requestCode){
+                case REQUEST_ENABLE_BT:
+                    if (resultCode == RESULT_OK) {
+
+                    }
+            }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(this, "msg", Toast.LENGTH_SHORT).show();
+    }
 
         class AuthenticationPagerAdapter extends FragmentPagerAdapter {
             private ArrayList<Fragment> fragmentList = new ArrayList<>();
@@ -44,4 +188,20 @@ public class MainActivity extends AppCompatActivity {
                 fragmentList.add(fragment);
             }
         }
-    }
+
+        private class Thread2 extends Thread {
+        public void run() {
+            for (int i = 0;i<50;i++) {
+                Message message = Message.obtain();
+                message.arg1 = i;
+                handler.sendMessage(message);
+
+                try {
+                    sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        }
+}
